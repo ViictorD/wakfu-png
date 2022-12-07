aafb34ad7fb24f8588c3a3b7ccca5f7fca637f14
Victor <victor.d34@pm.me>
2022-12-07 01:04:39 +0100

    fixs and 1.79 update

--- export_all.sh
+++ export_all.sh
@@ -1,10 +1,12 @@
 WAKFU_GAME_PATH=$1
 
-# cargo build --release
+cargo build --release
+
+./target/release/wakfu_png --path $WAKFU_GAME_PATH --map paper
 
 for f in $WAKFU_GAME_PATH/contents/maps/gfx/*.jar
 do
 	map_id=$(basename $f .jar)
-	./target/release/wakfu_png --path $WAKFU_GAME_PATH --map $map_id >> log
-	# ./target/release/wakfu_png --path $WAKFU_GAME_PATH --map $map_id --indoor
+	./target/release/wakfu_png --path $WAKFU_GAME_PATH --map $map_id
+	./target/release/wakfu_png --path $WAKFU_GAME_PATH --map $map_id --indoor
 done
